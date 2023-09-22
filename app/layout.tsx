1e2c362e846be291aba69899130158689c1730d9
CarlesRojas <carlesrojas@outlook.com>
2023-09-22 18:15:28 +0200

    next template

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import '@/app/globals.css';
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'One Pace',
+  description:
+    'One Pace is a fan project that recuts the One Piece anime in an endeavor to bring it more in line with the pacing of the original manga by Eiichiro Oda. The team accomplishes this by removing filler scenes not present in the source material.'
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+
+        <meta name="application-name" content="One Pace" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="One Pace" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="viewport"
+          content=" initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover minimum-scale=1"
+        />
+
+        <link rel="apple-touch-icon" href="/appleIcon120.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/appleIcon180.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/appleIcon152.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/appleIcon167.png" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
+
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
