a1e28dd3c833ed9903f8fcf6a348ef52b0b1e295
CarlesRojas <carlesrojas@outlook.com>
2023-10-01 11:14:50 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <link rel="apple-touch-icon" sizes="152x152" href="/appleIcon152.png" />
                 <link rel="apple-touch-icon" sizes="167x167" href="/appleIcon167.png" />
 
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:url" content="https://onepace.vercel.app" />
+                <meta name="twitter:title" content="One Pace" />
+                <meta
+                    name="twitter:description"
+                    content="One Pace is a fan project that recuts the One Piece anime in an endeavor to bring it more in line with the pacing of the original manga by Eiichiro Oda. The team accomplishes this by removing filler scenes not present in the source material."
+                />
+                <meta name="twitter:image" content="https://onepace.vercel.app./asset/icon/logo192.png" />
+                <meta name="twitter:creator" content="@PinyaGames" />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="One Pace" />
+                <meta
+                    property="og:description"
+                    content="One Pace is a fan project that recuts the One Piece anime in an endeavor to bring it more in line with the pacing of the original manga by Eiichiro Oda. The team accomplishes this by removing filler scenes not present in the source material."
+                />
+                <meta property="og:site_name" content="One Pace" />
+                <meta property="og:url" content="https://onepace.vercel.app" />
+                <meta property="og:image" content="https://onepace.vercel.app/asset/icon/logo192.png" />
+
                 <link rel="manifest" href="/manifest.json" />
                 <link rel="shortcut icon" href="/favicon.ico" />
             </head>
